858380d0b50ba2d635be6355484848febfddcc80
hilal211 <hilal1922001@gmail.com>
2021-09-27 10:50:24 +0300

    first committ

---
+++ routes/rating.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var ratingController = require('../controllers/ratingControllers')
+    router.get('/', ratingController.getAll);
+    // router.get('/:id', ratingController.get);
+    router.post('/', ratingController.post);
+    router.delete('/:id', ratingController.delete);
+    router.get('/getbysp/:id', ratingController.getBySp);
+    router.get('/v', ratingController.getStatistique);
+
+    
+
+    module.exports = router;
+
+
